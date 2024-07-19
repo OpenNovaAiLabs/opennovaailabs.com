7cf6b6073972bca9190157892528217e1a3a9a27
jamescheriyan <jamescheriyanco@gmail.com>
2024-07-19 18:09:36 +0100

    changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def hello():
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
